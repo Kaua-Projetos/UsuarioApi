9fb782600b530a373e6e290cbefd1a64358f2c1b
Kazito <kauaalexsandro31@gmail.com>
2025-04-24 23:01:16 -0400

    Update SecurityConfig.java

--- src/main/java/com/example/configuration/SecurityConfig.java
+++ src/main/java/com/example/configuration/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:4200",
-                "https://usuarioapi-young-brook-4416.fly.dev"
+                "https://gerenciadoruser.netlify.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
